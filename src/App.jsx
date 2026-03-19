36c048810fa2d914e07b0ed4960c410d30d342ed
Cymon Luther Mantaring <mantaringcymonluther@gmail.com>
2026-03-20 00:00:31 +0800

    feat(testimonials): complete testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ function App() {
         <Hero />
         <About />
         <Projects />
-        {/* 
         <Experience />
+        {/* 
         <Testimonials />
         <Contact /> */}
       </main>
